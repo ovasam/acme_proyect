c5e776de003c5c0fc9dab593d7ed2f36a72776dc
Samuel Ovalle <samuovalle2020@gmail.com>
2025-01-12 22:37:11 -0500

    Agregados mas mensajes de error, Limitando salidas en funciones

--- modules/error_messages.py
+++ modules/error_messages.py
@@ -1,15 +1,39 @@
-def invalid_option():
+# SPECIAL MESSAGE # 
+def adios():
     print('''
-===============================
-=       OPCION INVALIDA       =
-=      (use solo numeros)     =
-===============================
++++++++++++++++++++++++++++++
+++ Por favor Vuelva pronto ++
++++++++++++++++++++++++++++++
+          ''')
+
+
+def opcion_invalida():
+    print('''
++++++++++++++++++++++++++++++++
++       OPCION INVALIDA       +
++      (use solo numeros)     +
++++++++++++++++++++++++++++++++
                   ''')
 
-def out_of_range():
+def fuera_de_rango():
     print('''
-===============================
-=        FUERA DE RANGO       =
-=            (1-7)            =
-===============================
++++++++++++++++++++++++++++++++
++        FUERA DE RANGO       +
++            (1-7)            +
++++++++++++++++++++++++++++++++
                   ''')
+    
+
+def clave_incorrecta():
+    print('''
++++++++++++ ERROR +++++++++++
++    Contraseña Incorrecta  +
++++++++++++++++++++++++++++++
+          ''')
+    
+def no_opcion():
+    print('''
++++++++++++++++++++++++++++++++++++++++++
++   OPCION NO IMPLEMENTADA. LO SIENTO   +
++++++++++++++++++++++++++++++++++++++++++
+''')
