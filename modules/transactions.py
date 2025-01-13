95748a882f48ee01e27be9824efd4daf0eea935a
Lina Cardenas <lmcar2310@gmail.com>
2025-01-12 21:09:39 -0500

    Agregue la funcion retirar dinero

--- modules/transactions.py
+++ modules/transactions.py
@@ -1,23 +1,3 @@
-def consignar_dinero(id_cuenta, monto, arreglo):
-    if id_cuenta in arreglo: # Verificacion de que la cuenta existe
-        if monto > 0: 
-            print(f"Se consignó {monto} en la cuenta {id_cuenta}")
-            print(f"Su saldo actual es: {saldo}")
-        else:
-            print("Dinero insuficiente")
-    else:
-        print("La cuenta no existe")
-
-def retirar_dinero(id_cuenta, monto):
-    if id_cuenta in cuentas: # Verificacion de que la cuenta existe
-        if monto > 0: 
-            print(f"Se retiró {monto} en la cuenta {id_cuenta}")
-            print(f"Su saldo actual es: {saldo}")
-        else:
-            print("Dinero insuficiente")
-    else:
-        print("La cuenta no existe")
-
 # 4. Función: Retirar Dinero
 def retirar_dinero(n_cuenta, monto, cuentas):
     #Parámetros:
@@ -25,7 +5,25 @@ def retirar_dinero(n_cuenta, monto, cuentas):
     #monto (float): Monto de dinero a retirar.
     #cuentas (dict): Diccionario con las cuentas y sus datos.
 
-    #Verificar si la cuenta existe en el diccionario
+    #Verificar si la cuenta existe en el diccionario de cuentas
+    if n_cuenta in cuentas:
+        password = int(input("Ingrese su clave: "))
+        if password == cuentas[n_cuenta]:
+            saldo = cuentas[n_cuenta]
+            # Verificar si hay suficiente saldo para realizar el retiro
+            if monto > 0 and monto <= saldo:
+                cuentas[n_cuenta] -= monto  # Restar el monto del saldo
+                print(f"Se retiraron ${monto}")
+                print(f"Saldo actual: ${cuentas[n_cuenta]}")
+            elif monto > saldo:
+                print("Error: Fondos insuficientes.")
+            else:
+                print("Error: El monto debe ser mayor que 0.")
+        else:
+            print("Error: Clave incorrecta.")
+    else:
+        print("Error: La cuenta no existe.")
+
 
 
 def pagar_servicios2(id_cuenta, monto, servicio):
