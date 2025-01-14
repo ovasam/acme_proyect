7ad323e454608c4795c2c5b29280ba5fed5a7b89
Lina Cardenas <lmcar2310@gmail.com>
2025-01-13 20:09:39 -0500

    Cambiado ciclo.

--- modules/menus.py
+++ modules/menus.py
@@ -25,17 +25,24 @@ def menu_principal():
         return int(opcion)
     
 def sub_menu_servicios():
-        print("+++++++++++++++++++++++++")
-        print("+        SERVICIOS      +")
-        print("+++++++++++++++++++++++++")
-        print("+                       +")
-        print("+ 1. Energia            +")
-        print("+ 2. Gas                +")
-        print("+ 3. Agua               +")
-        print('+ 0. Salir              +')
-        print('+                       +')
-        print("+++++++++++++++++++++++++++")
-        opcion = int(input("¿Que desea hacer?: "))
-        while opcion < 0 or opcion > 3: #Validar que la opcion esté entre 1 y 3
-            opcion == int(input("Opcion invalida, eliga nuevamente"))
-        return opcion
+    print('''
++++++++++++++++++++++++++
++        SERVICIOS      +
++++++++++++++++++++++++++
++                       +
++ 1. Energia            +
++ 2. Gas                +
++ 3. Agua               +
++ 0. Salir              +
++                       +
++++++++++++++++++++++++++++
+''')
+    while True:
+        opcion = input('Ingrese una opcion > ')
+        if not opcion.isdigit():
+            opcion_invalida()
+            return menu_principal()
+        elif int(opcion) > 3 or int(opcion) <= 0:
+            fuera_de_rango()
+            return menu_principal()
+        return int(opcion)
