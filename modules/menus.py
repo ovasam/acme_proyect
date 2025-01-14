6b891b22ccc4de795d0d21ee8afc30b0bc55ed02
Samuel Torres <torressanchezsamuelfelipe@gmail.com>
2025-01-13 20:00:37 -0500

    Ágregados mas comentarios

--- modules/menus.py
+++ modules/menus.py
@@ -6,23 +6,24 @@ def menu_principal():
 +           A C M E  B A N K               +
 ++++++++++++++++++++++++++++++++++++++++++++          
 + 1. Crear Cuenta Nueva                    +
-+ 2. Consignar Dinero                      +
-+ 3. Retirar Dinero                        +
-+ 4. Pagar Servicios                       +
-+ 5. Mostrar Movimientos Bancarios         +
-+ 6. Informacion de Cuenta                 +
++ 2. Informacion de Cuenta                 +
++ 3. Consignar Dinero                      +
++ 4. Retirar Dinero                        +
++ 5. Pagar Servicios                       +
++ 6. Mostrar Movimientos Bancarios         +
 + 7. Salir                                 +
 ++++++++++++++++++++++++++++++++++++++++++++
 ''')
     while True:
         opcion = input('Ingrese una opcion > ')
-        if not opcion.isdigit():
-            opcion_invalida()
+        # Si la entrada no es un numero
+        if not opcion.isdigit(): # Verificar si la entrada contiene solo digitos
+            opcion_invalida() 
             return menu_principal()
         elif int(opcion) > 7 or int(opcion) <= 0:
             fuera_de_rango()
             return menu_principal()
-        return int(opcion)
+        return int(opcion) # Convierte la entrada a entero
     
 def sub_menu_servicios():
         print("+++++++++++++++++++++++++")
